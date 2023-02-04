069300dff03ac1e30f5754a233678084777eefa9
Korsak <korsak@MacBook-Air-Korsak.local>
2023-02-04 22:57:54 +0300

    add minisliders

---
+++ src/js/modules/slider/slider.js
@@ -0,0 +1,20 @@
+export default class Slider {
+    constructor ({container = null,
+        btns = null,
+        next = null,
+        prev =  null,
+        activeClass = null,
+        animate,
+        autoplay} = {}) {
+        this.container = document.querySelector(container);
+        this.slides = this.container.children;
+        this.btns = document.querySelectorAll(btns);
+        this.prev = document.querySelector(prev);
+        this.next = document.querySelector(next);
+        this.slideIndex = 1;
+        this.activeClass = activeClass;
+        this.animate = animate;
+        this.autoplay = autoplay;
+    }
+}
+
